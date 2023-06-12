1a29b9f339c86d1245b7d8026252c76ab72f58b6
Revan Midha <revi.midha@gmail.com>
2023-06-13 00:51:08 +0530

    Display changes and bug fixes

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,3 @@
-# This function adds two numbers
 import Addition
 import Subtraction
 import Multiplication
@@ -11,43 +10,53 @@ print("1.Addition")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
-print("5.Power")
+print("5.Power\n")
 
 while True:
     # take input from the user
     choice = input("Enter choice(1/2/3/4/5): ")
+    print()
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4','5'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
+            print()
         except ValueError:
-            print("Invalid input. Please enter a number.")
+            print("Invalid input. Please enter a number.\n")
             continue
 
         if choice == '1':
             print(num1, "+", num2, "=", Addition.add(num1, num2))
+            print()
 
         elif choice == '2':
             print(num1, "-", num2, "=", Subtraction.subtract(num1, num2))
+            print()
 
         elif choice == '3':
             print(num1, "*", num2, "=", Multiplication.multiply(num1, num2))
+            print()
 
         elif choice == '4':
             try:
                 print(num1, "/", num2, "=", Division.divide(num1, num2))
+                print()
             except:
-                print("Division by zero not allowed")
+                print("Division by zero not allowed\n")
 
         elif choice == '5':
             print(num1, "^", num2, "=", Power.power(num1, num2))
+            print()
         
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
+        print()
+        
+        if next_calculation in 'yY':
+            print("Thank for using our calculator!\n")
+            break
     else:
-        print("Invalid Input")
+        print("Invalid Input\n")
